db3895c97b19002e781eb8bac81e286d63d68227
lucastadeu50 <lucastadeu50@hotmail.com>
2018-03-31 16:11:00 -0300

    Permissoes

--- app/src/main/java/com/example/dimasjose/myapplication/control/MainActivity.java
+++ app/src/main/java/com/example/dimasjose/myapplication/control/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.dimasjose.myapplication.control;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.dimasjose.myapplication.R;
 //TESTE
+
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonConsultar; //decclara os botões
@@ -20,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         buttonConsultar = (Button)findViewById(R.id.buttonConsultar); // faz a ligaçao entre o botao no XML e o codigo que determinará sua ação
         buttonIncluir = (Button)findViewById(R.id.buttonIncluir);
+
+
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                             Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                             Manifest.permission.RECORD_AUDIO},
+
+                1);
+
+
+
+
 
         //        buttonIncluir.setOnClickListener(new View.OnClickListener() {
         // Coloca um ouvinte no botão, toda vez que tiver um click ele executa a açao que esta dentro da função
@@ -40,5 +59,28 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permissão Garantida", Toast.LENGTH_LONG).show();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
