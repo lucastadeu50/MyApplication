ef00c94c69bc72c007ed6e62cd45f0872c568a84
lucastadeu50 <lucastadeu50@hotmail.com>
2018-04-07 18:09:12 -0300

    07/04

--- app/src/main/java/com/example/dimasjose/myapplication/control/TestePertubacaoActivity.java
+++ app/src/main/java/com/example/dimasjose/myapplication/control/TestePertubacaoActivity.java
@@ -2,6 +2,7 @@ package com.example.dimasjose.myapplication.control;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,8 +16,10 @@ import java.io.IOException;
 public class TestePertubacaoActivity extends AppCompatActivity {
     Button buttonAncora, buttonAmostra, buttonMaior,buttonIgual, buttonMenor;
 
-    MediaPlayer amostra,gravacao;
-
+    MediaPlayer amostra1,amostra2,amostra3,amostra4,amostra5,amostra6,amostra7,gravacao;
+    MediaPlayer[] mediaPlayer;
+    int i = 0;
+    AlertDialog resultado;
 
 
     @Override
@@ -28,19 +31,37 @@ public class TestePertubacaoActivity extends AppCompatActivity {
         buttonMaior = findViewById(R.id.buttonMaior);
         buttonMenor = findViewById(R.id.buttonMenor);
         buttonIgual = findViewById(R.id.buttonIgual);
-        amostra = MediaPlayer.create(TestePertubacaoActivity.this, R.raw.snr0210j020s000vaf220_1);
+        amostra1 = MediaPlayer.create(TestePertubacaoActivity.this, R.raw.snr0210j020s000vaf220_1);
+        amostra2 = MediaPlayer.create(TestePertubacaoActivity.this, R.raw.snr0350j020s000vaf220_1);
+        amostra3 = MediaPlayer.create(TestePertubacaoActivity.this, R.raw.snr0070j020s000vaf220_1);
+        amostra4 = MediaPlayer.create(TestePertubacaoActivity.this, R.raw.snr0420j020s000vaf220_1);
+        amostra5 = MediaPlayer.create(TestePertubacaoActivity.this, R.raw.snr0280j020s000vaf220_1);
+        amostra6 = MediaPlayer.create(TestePertubacaoActivity.this, R.raw.snr0140j020s000vaf220_1);
+        amostra7 = MediaPlayer.create(TestePertubacaoActivity.this, R.raw.snr0010j020s000vaf220_1);
+        mediaPlayer = new MediaPlayer[]{amostra1, amostra2, amostra3, amostra4, amostra5, amostra6, amostra7};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Resultado do teste");
+        builder.setMessage("A amostra" + Integer.toString(i));
+
+
+
+
 
 
         Intent intent = getIntent();
         final String OUTPUT_FILE = intent.getStringExtra("gravacao");
 
-        Toast.makeText(TestePertubacaoActivity.this, OUTPUT_FILE, Toast.LENGTH_SHORT).show();
 
 
         buttonAncora.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                amostra.start();
+                if (i>=0 && i<7) {
+                    mediaPlayer[i].start();
+                }else{
+                    Toast.makeText(TestePertubacaoActivity.this, "Erro ao reproduzir ancora", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         buttonAmostra.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +77,36 @@ public class TestePertubacaoActivity extends AppCompatActivity {
                 gravacao.start();
             }
         });
+        buttonMenor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = 2*i+1;
+                Toast.makeText(TestePertubacaoActivity.this, Integer.toString(i), Toast.LENGTH_LONG).show();
+                if(i==4){
+                    buttonMenor.setEnabled(false);
+                }
+            }
+        });
+
+        buttonIgual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(TestePertubacaoActivity.this, Integer.toString(i), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        buttonMaior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = 2*i+2;
+                Toast.makeText(TestePertubacaoActivity.this, Integer.toString(i), Toast.LENGTH_LONG).show();
+                if (i==13){
+                    buttonMaior.setEnabled(false);
+                }
+            }
+        });
 
     }
 }
+
